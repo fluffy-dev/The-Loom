0eee74a680ddd9ee306a2fd7a93f43bf935e9220
Rauan <asetovrauan95@gmail.com>
2025-06-29 21:14:36 +0500

    added connection between rooms and users

--- backend/user/models/user.py
+++ backend/user/models/user.py
@@ -1,5 +1,6 @@
+from typing import List
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from backend.libs.base_model import Base
 
@@ -14,3 +15,6 @@ class UserModel(Base):
     login: Mapped[str] = mapped_column(String(50), unique=True, index=True)
     email: Mapped[str] = mapped_column(String(50), unique=True, index=True)
     password: Mapped[str] = mapped_column(String(255))
+
+    owned_rooms: Mapped[List["RoomModel"]] = relationship(back_populates="owner")
+    participated_rooms: Mapped[List["RoomParticipantModel"]] = relationship(back_populates="user")
