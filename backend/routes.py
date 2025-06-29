c5e168cf82ddcf71c2f224a69b22c2946a15425a
Rauan <asetovrauan95@gmail.com>
2025-06-29 21:07:52 +0500

    added auth router to main entry point

--- backend/routes.py
+++ backend/routes.py
@@ -1,5 +1,10 @@
 from fastapi import APIRouter
 from backend.user.router import router as user_router
+from backend.auth.router import router as auth_router
+
 
 router = APIRouter(prefix="/api")
+
+
+router.include_router(auth_router)
 router.include_router(user_router)
