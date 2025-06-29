0a8ff44cc7d2486d5e990daff8957fcc49bd3c0a
Rauan <asetovrauan95@gmail.com>
2025-06-29 21:20:00 +0500

    room router added to main router

--- backend/routes.py
+++ backend/routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from backend.user.router import router as user_router
 from backend.auth.router import router as auth_router
+from backend.room.router import router as room_router
 
 
 router = APIRouter(prefix="/api")
@@ -8,3 +9,4 @@ router = APIRouter(prefix="/api")
 
 router.include_router(auth_router)
 router.include_router(user_router)
+router.include_router(room_router)
